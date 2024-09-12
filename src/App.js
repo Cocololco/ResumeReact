740dca5f9f0a553db0bed344b3d2a727055c2e18
Cocololco <116242938+Cocololco@users.noreply.github.com>
2024-09-12 13:33:02 +1000

    add google tag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import React from 'react';
 import AppRoutes from './routes/AppRoutes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import TagManager from 'react-gtm-module';
 
+const tagManagerArgs = {
+  gtmId: 'G-WH3NT1ZL0W' 
+};
+TagManager.initialize(tagManagerArgs);
 function App() {
   return (
     <AppRoutes />
